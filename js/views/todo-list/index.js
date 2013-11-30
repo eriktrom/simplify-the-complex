74755871393fa2bc0916a0229c8ada1c61d92bce
Erik Trom <erik.trom@gmail.com>
2013-11-30 11:23:10 -0800

    add todo-list events hash

--- js/views/todo-list/index.js
+++ js/views/todo-list/index.js
@@ -3,9 +3,24 @@ define([
   'collections/todos',
   'hbs!templates/todo-list/index'
 ], function (View, todos, template) {
+
+  var events = {
+    "submit form": function(event) {
+      event.preventDefault();
+      var attrs = this.serialize();
+      this.collection.add(attrs);
+      this.$('input[name="title"]').val('');
+    },
+    'change input[type="checkbox"]': function(event) {
+      var model = $(event.target).model();
+      model.set({done: event.target.checked});
+    }
+  };
+
   return new View({
     name: 'todo-list/index',
     template: template,
-    collection: todos
+    collection: todos,
+    events: events
   });
 });
